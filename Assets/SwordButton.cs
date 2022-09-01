9cd32fd2540eb53704d070d4c10607ce331c31e2
jmjeon8258 <jmjeon9923@gmail.com>
2022-09-02 01:41:02 +0900

    기본셋

---
+++ Assets/SwordButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordButton : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Sword;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void SwordSnap()
+    {
+        
+    }
+}
